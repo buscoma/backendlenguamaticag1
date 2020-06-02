de3e01524d8c5251aad078f532ed13c20c8e2e66
pablodnd <pablogaston1989@gmail.com>
2020-06-01 21:05:24 -0300

    merge master en mi branch

--- routes/api.js
+++ routes/api.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const express = require("express");
 const UserController = require("../controllers/users");
 const users = require("./api/users");
@@ -6,7 +5,7 @@ const comprensionLectora = require("./api/comprensionLectora");
 const juegoNumAPalabra = require("./api/juegoNumAPalabra");
 const palabrasPerdidas = require("./api/palabrasPerdidas");
 const authenticateJWT = require("../auth/authenticateJWT");
-
+let player = require('./api/player');
 const router = express.Router();
 
 // Authentication
@@ -15,23 +14,6 @@ router.post("/login", UserController.loginUser);
 
 // Users api
 router.use("/users", users);
-=======
-let express = require('express');
-let UserController = require('../controllers/users');
-let users = require('./api/users');
-let comprensionLectora = require('./api/comprensionLectora');
-let player = require('./api/player');
-let authenticateJWT = require('../auth/authenticateJWT');
-
-let router = express.Router();
-
-// Authentication
-router.post('/registration', authenticateJWT, UserController.createUser);
-router.post('/login', UserController.loginUser);
-
-// users api
-router.use('/users', users);
->>>>>>> master
 
 // compresionLectora api
 router.use("/comprensionLectora", comprensionLectora);
--- services/mongo/comprensionLectora.js
+++ services/mongo/comprensionLectora.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 var ComprensionLectora = require("../../models/mongo/comprensionLectora");
 var helper = require('../helpers/helpers');
 
@@ -9,17 +8,6 @@ exports.getNivel = async function (nivel) {
     try {
         var nivel = await ComprensionLectora.find(query);
         return helper.getRandomDocument(nivel);
-=======
-let ComprensionLectora = require("../../models/mongo/comprensionLectora");
-
-exports.getNivel = async function (nivel) {
-    let query = {
-        nivel: nivel
-    };
-    try {
-        let nivel = await ComprensionLectora.find(query);
-        return nivel;
->>>>>>> master
     } catch (e) {
         throw Error("Error al recuperar el nivel de ComprensionLectora");
     }
